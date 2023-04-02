24336ef3a1d84f1d0254304cae48bd0a937beb82
Damla KS <damlaksrbst@gmail.com>
2023-04-03 02:58:45 +0300

    first commit

---
+++ TheRichestPeople/FirstView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstView.swift
+//  TheRichestPeople
+//
+//  Created by Damla KS on 1.04.2023.
+//
+
+import UIKit
+
+class FirstView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
